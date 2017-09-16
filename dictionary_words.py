ff597edfe9fb72a115fee8fa09a858a525bc4acc
Jeffchiucp <jeffchiu2000@gmail.com>
2017-09-15 23:28:39 -0700

    include word-analyze.py

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,18 +1,25 @@
 import random
 import sys
 import linecache
+import time
+
+"""
 #we're going to use a text file that is already on your computer and is already formatted nicely for parsing: the words file. This file is available on all Unix and Unix-like systems.
 #On my MacBook running OS X Yosemite, this file is located at /usr/share/dict/words.
-# it has over 235886 words
-
+ it has over 235886 words
+The linecache module allows one to get any line from a Python source file, while attempting to optimize internally, using a cache, the common case where many lines are read from a single file. This is used by the traceback module to retrieve source lines for inclusion in the formatted traceback.
+The method rstrip() returns a copy of the string in which all chars have been stripped from the end of the string (default whitespace characters).
+"""
 def randomSentence(): #6
     filename = "/usr/share/dict/words"
-    #array = file.read().splitlines()
+
+    # get the random index and use the linecache to get the word
     rand_index = random.randint(1, 235886)
     word = linecache.getline(filename, rand_index, module_globals=None)
     word = word.rstrip()
+    # return all of the string without space
+    # added the white space
     return word + " "
-
 
 if __name__ == '__main__': #5
     count = int(sys.argv[1])
--- rearrange.py
+++ rearrange.py
@@ -1,3 +1,4 @@
+#!python3
 import random
 import sys
 
@@ -5,24 +6,52 @@ import sys
 no use of slices but working on various problem
 """
 
+#passed by reference
+# passed by value
+# passed it here
+# .copy() # python 3 only
+# .clone in Java
+# list() method, list
+# scramblede values of the original list  list(original)
+#  does the original list get modified?
+#  yes. in this case.
+# we should do copy a new list. please implement that.
+# 1 way. .copy()
+# scrambled = original[:]
+# is scrambled list, is it
+
+# thing to think about ...
+# objects, and collections - in python, treated it like passed by reference
+# int, float - in python, treated like passed by value. They can be copied as a pointer.
+
+# syntax sugar of the python
+# go all the [6::-1]
+# [::-1] easy way to reverse the list
+# scrambled = original[::-1]
 def random_shuffle(): #2
     # Creates a random number between 1 and the total number of tokens
     words = sys.argv[1:len(sys.argv)]
-
+    #python sorted, and added color
+    # python has a way for a way to shuffle index
+    # len(includes original, and excludes the length )
+    # python | if you don't know,
+    # alice in
     rand_index = random.randint(1, 20)
 
+    # before we learn more about
+    # slice, range
+    # make a list of integer
+    # first argument, inclusive,
     for num in range(rand_index):
         first_index = random.randint(0,  len(words) - 1)
         second_index = random.randint(0,  len(words) - 1)
-        # swap the word
-        temp = words[first_index]
-        words[first_index] = words[second_index]
-        words[second_index] = temp
+        words[first_index], words[second_index] = words[second_index], words[first_index]
+        # words[second_index] = temp
     return words
-
-
-
-if __name__ == '__main__':
-    print (' '.join(random_shuffle()))
+    # words is a string1
     # print out the function to debug
-    # print (random_word_order())
+
+    #run on the command line
+if __name__ == '__main__':
+    print(' '.join(random_shuffle()))
+    # print out the function to debug
