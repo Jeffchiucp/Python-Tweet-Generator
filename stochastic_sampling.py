bf2cc5d7fc948a3c2e7f1efa35089e8ba33c1346
Jeffchiucp <jeffchiu2000@gmail.com>
2017-09-21 17:19:43 -0700

    implementing weight frequencies

--- stochastic_sampling.py
+++ stochastic_sampling.py
@@ -1,5 +1,6 @@
 import sys
 import re
+#from histogram import text_file_list
 import random
 from datetime import datetime
 
@@ -14,43 +15,52 @@ def histogram(source_text):
     # python ways of removing any sort of string, removing ,:;, and any other special character
     for word in source_text.split():
         word = re.sub('[.,:;!-[]?', '', word)
-
         if word in histogram:
             histogram[word] += 1
         else:
             histogram[word] = 1
     return histogram
 
-def random_word(histogram):
-    """   first take a random sample of key/value pairs
-    and add them to a list """
-    probability = 0
-    rand_index = random.randint(1, sum(histogram.values()))
-    # Algorithm 1
-    for word in histogram:
-        probability += histogram[word]
-        if probability >= rand_index:
+def get_random_word(dictionary):
+    """Returning a random word from a dictionary. thanks to Elmer """
+    rand_index = random.randint(0, len(dictionary) - 1)
+    key_list = list(dictionary)
+    random_w = key_list[rand_index]
+    return random_w
+
+def get_random_with_weight(histogram):
+    """Randomly picking items with given weights. Code Source from Elmer"""
+    dictionary_weightValue = {}
+    total_values = sum(histogram.values())
+    # Grab every value from dictionary python
+    for (word, value) in histogram.items():
+        print("{} : {} ".format(word, value))
+        total = float(value / total_values)
+        # Getting the weight of each value
+        #  weighted_value
+        dictionary_weightValue[word] = total
+    return dictionary_weightValue
+
+def get_random_word_by_weight_prob(dictionary_weightValue):
+    """
+    first take a random sample of key/value pairs
+    floating point, get ferequency
+    """
+    random_int = random.random()
+    cumulative_probability = 0.0
+    for (word, weightValue) in dictionary_weightValue.items():
+        # computing the increasing cumulative probability
+        cumulative_probability += weightValue
+        # until the cumulative_probability becomes greater than the random_int
+        if random_int < cumulative_probability:
             if word in outcome_gram:
                 outcome_gram[word] += 1
-            else:
-                outcome_gram[word] = 1
-            return word
-    # Algorithm 1
-    # for (key, value) in histogram.items():
-    #     for num in range(1, value + 1):
-    #         if probability == rand_index:
-    #             if key in outcome_gram:
-    #                 outcome_gram[key] += 1
-    #             else:
-    #                 outcome_gram[key] = 1
-    #             # return outcome_gram
-    #             return key
-    #         else:
-    #             probability += 1
+            break
+    return word
 
 def unique_words(histogram):
     """Return the amount of words that only show up once in the histogram."""
-    #  uniqueWords is a list that stores the words that only happens once
+    #  Source code from James. uniqueWords is a list that stores the words that only happens once
     uniqueWords = []
     for histogramWord in histogram:
         if histogramWord[1] == 1:
@@ -60,24 +70,31 @@ def unique_words(histogram):
 def frequency(word, histogram):
     """Return the amount of times a word shows up in the histogram."""
     for histogramWord in histogram:
-        if histogramWord[0] == word:
+        if histogramWord[word] == 1:
             return histogramWord[1]
 
 if __name__ == "__main__":
     outcome_gram = {}
-    dict = open("./sample-text.txt", 'r')
+    dict = open('./fish.txt', 'r')
     text = dict.read()
     dict.close()
     hist_dict = histogram(text)
-    for number in range(1, 100000):
-        random_word(hist_dict)
+    weight = get_random_with_weight(hist_dict)
+    # for number in range(1, 100000):
+    print(get_random_word_by_weight_prob(weight))
 
-    print("Original: ")
-    #print(random_word(dict))
-    print("If this were a perfect algorithm, the number of fish would be 50000, but my actual value is " + str(outcome_gram["fish"]))
+    start = datetime.now()
+    dictionary_weightValue = get_random_with_weight(hist_dict)
+    #print("If this were a perfect algorithm, the number of fish would be 50000, but my actual value is " + str(outcome_gram["fish"]))
     # for word, expected_count in hist_dict.items():
-    print("The percent error is " + str(abs(outcome_gram["fish"] - 50000.0) / 50000.0 * 100.0) + "%")
-    # outcome_gram["fish"] = abs(outcome_gram["fish"] - 5000.0) / 5000.0 * 100.0
+    #print("The percent error is " + str(abs(outcome_gram["fish"] - 50000.0) / 50000.0 * 100.0) + "%")
+    #outcome_gram["fish"] = abs(outcome_gram["fish"] - 5000.0) / 5000.0 * 100.0
     # calculate the frequencies of the words
     print (outcome_gram)
+    # for word, expected_count in hist_dict.items():
+    #print(unique_words(histogram))
+    print("calculate the frequencies of the words")
+
+    # calculate the frequencies of the words
+    ##print(unique_words())
     print("Time: " + str(datetime.now() - start))
