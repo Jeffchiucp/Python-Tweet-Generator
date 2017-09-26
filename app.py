cbcb5db6b702cb5bf3d987b317f1c86c260b21db
Jeffchiucp <jeffchiu2000@gmail.com>
2017-09-26 12:00:11 -0700

    added CSS styles

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 #!python3
 """main script, uses other modules to generate sentences"""
 from random import randint
-from flask import Flask
+from flask import Flask, request, render_template
 from python_script import sample
-
+import time
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return sample.generate_sentence('./text/output_data.txt', randint(10, 15))
+# @app.route('/')
+# def index():
+#     return sample.generate_sentence('./text/output_data.txt', randint(10,15))
 
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+
+    tweet = sample.generate_sentence('./text/output_data.txt', randint(10,15))
+    if request.method == 'GET':
+        return render_template('index.html', tweet=tweet, time=time.time)
 
 if __name__ == "__main__":
     app.run()
