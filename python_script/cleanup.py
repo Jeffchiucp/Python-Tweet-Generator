6b83b387c74debbed392713b52c106604f34e4c1
Jeffchiucp <jeffchiu2000@gmail.com>
2017-10-03 18:36:54 -0700

    implemented the markov chain models

--- python_script/cleanup.py
+++ python_script/cleanup.py
@@ -2,9 +2,37 @@ import re
 
 # Takes: Freshly split list | Returns: Clean list of words
 """cleannig up files for the raw text. Credit from Sam and Elmer"""
-def clean_text(dirty_list):
-    clean_list = []
-    for word in dirty_list:
-        word = re.sub('[.,:;!-[]?', '', word)
-        clean_list.append(word)
-    return clean_list
+import sys
+import re
+
+
+def clean_file(filename):
+    data_file = open(filename, 'r')
+    words_list = data_file.read().lower()
+    remove_punctuation(words_list)
+    result_list = []
+
+    matches = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", words_list)
+    for match in matches:
+        result_list.append(match)
+    return result_list
+# added a new line
+
+def remove_punctuation(text):
+    no_punc_text = re.sub('[,.()]', '', text)
+    no_punc_text = re.sub('--', ' ', no_punc_text)
+    no_punc_text = re.sub(':', ' ', no_punc_text)
+    return no_punc_text
+
+def main():
+    user_argument_count = len(sys.argv)
+    if user_argument_count == 1:
+        print ('Error: textfile not provided')
+    else:
+        data_file = open(sys.argv[1], 'r')
+        words_list = data_file.read().lower()
+        # print words_list
+
+        matches = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", words_list)
+        for match in matches:
+            print (match)
--- python_script/histogram_oop.py
+++ python_script/histogram_oop.py
@@ -1,16 +1,13 @@
 #!python
 
+from __future__ import division
+import random
 
 
 class Dictogram(dict):
-        """ A Dictogram is a custom data type we use to store data for our Markov Chain. We use key (a word or phrase) then
-    a histogram (of following words or phrases) to store our data.
-    A key is the current word or phrase we are looking at (it can be
-    a word of phrase because of the order we are using). A value is the word or phrase following our current key.
-     """
 
     def __init__(self, iterable=None):
-        """Initialize this histogram as a new dict; update with given items ***"""
+        """Initialize this histogram as a new dict; update with given items"""
         super(Dictogram, self).__init__()
         self.types = 0  # the number of distinct item types in this histogram
         self.tokens = 0  # the total count of all item tokens in this histogram
@@ -31,44 +28,115 @@ class Dictogram(dict):
     def count(self, item):
         """Return the count of the given item in this histogram, or 0"""
         # TODO: retrieve item count
-        if item in self:
-           return self[item]
-        else:
-            return 0 
+        return self.get(item, 0)
+
+    def return_random_word(self):
+        """return random word"""
+        random_key = random.sample(self, 1)
+        return random_key[0]
+
+    def return_weighted_random_word(self):
+        #return random word from the weight probabilities
+        random_weight = random.randint(0, self.tokens-1)
+        #test index
+        index = 0
+        words_list = list(self.keys())
+        # uncommeting to test the type
+        # print('keys:', words_list)
+        # print('type:', type(words_list))
+        # for i in range(0, self.types):
+        #     word = words_list[i]
+        for word in words_list:
+            index += self[word]
+            # print (index)
+            if(index > random_weight):
+                return word
+
+
+class Listogram(list):
+
+    def __init__(self, iterable=None):
+        """Initialize this histogram as a new list; update with given items"""
+        super(Listogram, self).__init__()
+        self.types = 0  # the number of distinct item types in this histogram
+        self.tokens = 0  # the total count of all item tokens in this histogram
+        if iterable:
+            self.update(iterable)
+
+    def update(self, iterable):
+        """Update this histogram with the items in the given iterable"""
+        temp_dict = {}
+        for item in iterable:
+            # TODO: increment item count
+            self.tokens += 1
+            if item in temp_dict:
+                temp_dict[item] += 1
+            else:
+                temp_dict[item] = 1
+
+        self[0:] = temp_dict.items();
+
+        self.types = len(self)
+
+    def count(self, item):
+        """Return the count of the given item in this histogram, or 0"""
+        # TODO: retrieve item count
+        for tup in self:
+            if tup[0] == item:
+                return tup[1]
+        return 0
+
+    def __contains__(self, item):
+        """Return True if the given item is in this histogram, or False"""
+        # TODO: check if item is in histogram
+        for tup in self:
+            if tup[0] == item:
+                return True
+        return False
+
+    def _index(self, target):
+        """Return the index of the (target, count) entry if found, or None"""
+        # TODO: implement linear search to find an item's index
+        for index, tup in enumerate(self):
+            if tup[0] == item:
+                return index
+        return None
 
 
 def test_histogram(text_list):
-    print('text list:', text_list)
+    pass
+    # print('text list:', text_list)
 
-    hist_dict = Dictogram(text_list)
-    print('dictogram:', hist_dict)
-
-    hist_list = Listogram(text_list)
-    print('listogram:', hist_list)
+    # hist_dict = Dictogram(text_list)
+    # print('dictogram:', hist_dict)
 
 
 def read_from_file(filename):
     """Parse the given file into a list of strings, separated by seperator."""
     return file(filename).read().strip().split()
 
+def list_of_words(length):
+    dict_words = '/usr/share/dict/words'
+    words_str = open(dict_words, 'r').read()
+    all_words = words_str.split("\n")
+    return all_words[0:length]
+
 
 if __name__ == '__main__':
     import sys
     arguments = sys.argv[1:]  # exclude script name in first argument
-    start = time.time()
-    #     # test hisogram on letters in a word
-    #     word = 'abracadabra'
-    #     test_histogram(word)
-    #     print()
-    #     # test hisogram on words in a sentence
-    #     sentence = 'one fish two fish red fish blue fish'
-    #     word_list = sentence.split()
-    #     test_histogram(word_list)
-    # elif len(arguments) == 1:
-    #     # test hisogram on text from a file
-    #     filename = arguments[0]
-    #     text_list = read_from_file(filename)
-    #     test_histogram(text_list)
-    # else:
-    #     # test hisogram on given arguments
-    #     test_histogram(arguments)
+    #uncomment to test the data and word
+    # data = ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish']
+    # gram = Dictogram(data)
+    # for word in data:
+    #     gram.update(word)
+
+    # dd = {}
+
+    # for i in range(10):
+    #     word = gram.return_random_word()
+    #     if word in dd:
+    #         dd[word] += 1
+    #     else:
+    #         dd[word] = 1
+    # print(dd)
