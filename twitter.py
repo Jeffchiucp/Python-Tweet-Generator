7c34593aa382335d903a5ed54dd77969e57a119a
Jeffchiucp <jeffchiu2000@gmail.com>
2017-10-12 19:25:16 -0700

    added twitter api

---
+++ twitter.py
@@ -0,0 +1,35 @@
+import os
+import dotenv
+dotenv.load_dotenv('.env')
+
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+#use the Twitter API authorized session
+from requests_oauthlib import OAuth1Session
+
+session = OAuth1Session(consumer_key,
+                        client_secret=consumer_secret,
+                        resource_owner_key=access_token,
+                        resource_owner_secret=access_token_secret)
+
+# The URL endpoint to update a status (i.e. tweet)
+url = 'https://api.twitter.com/1.1/statuses/update.json'
+
+# The contents of status (i.e. tweet text)
+status = 'Hello World!'
+
+# Send a POST request to the url with a 'status' parameter
+resp = session.post(url, { 'status': status })
+
+# Show the text from the response
+# print(resp.text)
+
+def tweet(status):
+    resp = session.post(url, { 'status': status })
+    return resp.text
+
+if __name__ == "__main__":
+    tweet("testing")
