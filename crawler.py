ff87bb275298c753e593789f451b27d4d402c1ac
Jeffchiucp <jeffchiu2000@gmail.com>
2017-10-15 14:12:40 -0700

    Creating a bigger Corpus #9

---
+++ crawler.py
@@ -0,0 +1,30 @@
+import requests
+
+DIFFBOT_API_URL = 'http://api.diffbot.com/v3/article'
+DIFFBOT_DEV_TOKEN = 'e264b8d5a09de9aa40574ed1eba99eb1'
+
+def get_article(article_url):
+    # set request params for API request
+    params = { 'token': DIFFBOT_DEV_TOKEN,
+               'url': article_url,
+               'discussion': 'false' }
+
+    res = requests.get(DIFFBOT_API_URL, params) # hit the Diffbot API
+    res_obj = res.json()['objects'][0]          # parse the response object
+
+    return res_obj['text']                      # pull out the text
+
+if __name__ == '__main__':
+    import sys
+    urls_file = open(sys.argv[1])
+    output_file = open('corpus.txt', 'w')
+
+    corpus = ''
+
+    for line in urls_file:
+        url = line.strip() # remove leading/trailing whitespace
+        article = get_article(url)
+        corpus += article
+
+    output_file.write(corpus)
+    print('Corpus saved to {}'.format(output_file.name))
