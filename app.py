8904edf27e5b50b0a466428e842fa67c38e7100e
Jeffchiucp <jeffchiu2000@gmail.com>
2017-10-19 08:42:44 -0700

    Database cleanup resolve issue #4 #8

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 #!python3
 """main script, uses other modules to generate sentences"""
-# from python_script import markov
-# from python_script import Histogram_oop 
-# from Histogram_oop import Dictogram
 from random import randint
 from flask import Flask, request, jsonify, render_template, json
-import markov
 import cleanup
+import markov
 import twitter
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Table
-import twitter
+import psycopg2
+import sys
 from dictogram import Dictogram
 import time
 import os
 
-"""Use SQLAlchemy database to store current tweets in the sqlite db 
+""" Code changes 
+Remove SQLAlchemy (SQLlite3)database and use Postgres Database instead
+Heroku supports Postgres store current tweets in the sqlite db 
 tweets is the list of tweet storage for the tweet string generating
-old route / handle both Get and Post request
-index handles the Get request for Word Generation when we refresh the page
-new routes
+# Create an Postgres Table Tweet
+old route / handle both Get and Post request index handles 
+the Get request for Word Generation 
+changes the routes so user don't have to refresh every time 
 route /tweet handles the post request for the twitter API
 route /favorites handles all the favorite tweet data
 route / get_tweet helps to get new tweet when the button is being clicked
@@ -27,23 +26,19 @@ route / get_tweet helps to get new tweet when the button is being clicked
 """
 
 app = Flask(__name__, instance_relative_config=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-tweets = []
-file_name = './text/complete_cleaned.txt'
+file_name = './text/shakespeare.txt'
 cleaned_file = cleanup.clean_file(file_name)
 markov_chain = markov.markov_model(cleaned_file)
-class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    content = db.Column(db.String(200), unique=True)
 
-    def __init__(self, content):
-        self.content = content
+#Postgres library to connect to Heroku DB
+#tweet is the list of tweet stored for the twitter API routing 
+conn = psycopg2.connect("dbname=d2bggd7r2dh3bk user=mjbapfvphmennc password=" +
+                        os.environ.get('DATABASE_PASSWORD') + " host=" + os.environ.get('DATABASE_HOST'))
+cur = conn.cursor()
+cur.execute("SELECT * FROM Tweets;")
+tweets = []
 
-    def __repr__(self):
-        return '<Tweet %r>' % self.content
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -55,12 +50,6 @@ def index():
 
         return render_template('index.html', tweet=tweet, time=time.time)
     # how to store them in the database? I'm not sure
-    #uncomment the lines to take of the Sqlite feature
-    # elif request.method == 'POST':
-    #     # if currentTweet not in db:
-    #     addFavoriteTweet(currentTweet)
-    #     return render_template('index.html', tweet=currentTweet, time=time.time)
-
 
 """main script, uses various different routes modules to generate sentences"""
 # cal the tweet method
@@ -71,13 +60,12 @@ def tweet():
     # noinspection PyBroadException
     try:
         twitter.tweet = tweets[body['tweet']]
-        print(twitter.tweet)
-        print(jsonify)
+        twitter.tweet(tweet_value)
+        # print(twitter.tweet)
         return jsonify({
             "success": True
         })
     except Exception as e:
-        print(jsonify)
         return jsonify({
             "success": False
         })
@@ -107,36 +95,30 @@ def get_favorite_tweets():
         "data": favorites
     })
 
-@app.route('/get_tweet',methods=['GET', 'POST'])
+@app.route('/new',methods=['POST'])
 def generate_new_tweet():
-
     sentence = markov.generate_sentence(15, markov_chain)
 
-    tweets_list = Tweet.query.all()
-    tweet_strings = []
-    print("Printing all content")
-    for tweet in tweets_list:
-        tweet_strings.append(tweet.content)
-    print(jsonify)
+    body = json.loads(request.data)
+
+    words = body['words']
+
+    sentence = None
+    if words is None:
+        sentence = markov.generate_sentence(15, markov_chain)
+    else:
+        try:
+            sentence = markov.generate_sentence(15, markov_chain)
+        except TypeError:
+            sentence = markov.generate_sentence(15, markov_chain)
+
+    tweets.append(sentence)
     return jsonify({
         "success": True,
         "data": sentence,
+        "id": len(tweets) - 1
     })
 
-def addFavoriteTweet(tweet_content):
-    tweet = Tweet(tweet_content)
-    db.session.add(tweet)
-    db.session.commit()
-    print(Tweet.query.all())
-
-def clear_data(session):
-    meta = db.metadata
-    for table in reversed(meta.sorted_tables):
-        print ('Clear table %s' % table)
-        session.execute(table.delete())
-    session.commit()
 
 if __name__ == "__main__":
-    db.create_all()
-
     app.run(debug = True)
